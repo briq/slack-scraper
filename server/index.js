3309b4809f4ede01462143b141f3851f7f17eb91
Thomas Groutars <tgroutars@gmail.com>
2017-10-18 15:17:42 +0200

    Fix vew directory

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const routes = require('./routes');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 
 app.use(compression());
